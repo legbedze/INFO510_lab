18205036bbd78753bdda99a5b640c1ec6fc40a41
legbedze <legnedze.plegbedze.p@northeastern.edu>
2024-10-03 14:05:52 -0400

    Added the java swing application for patient registration and view

---
+++ LAB3/Lab3_Navigation/src/model/Patient.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author H_Kofi
+ */
+public class Patient {
+
+private String firstname;
+private String lasttname;
+private String age;
+private String phonenumber;
+private String emailAddress;
+private String gender;
+private String patientType;
+private String comment;
+private Date dateOfBirth;
+private ImageIcon profilePicture;
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+  
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLasttname() {
+        return lasttname;
+    }
+
+    public void setLasttname(String lasttname) {
+        this.lasttname = lasttname;
+    }
+
+
+ 
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+
+    
+}
